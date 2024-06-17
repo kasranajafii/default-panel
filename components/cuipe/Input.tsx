43fd4835ed68b392927398ba72c8023c353043fe
kasranajafii <kasranajafi40@gmail.com>
2024-06-17 12:10:32 +0330

    fixed crip input problems

--- components/cuipe/Input.tsx
+++ components/cuipe/Input.tsx
@@ -1,9 +1,9 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, HTMLInputTypeAttribute } from "react";
 
 export type Tprops = {
     id?: string;
     name?: string;
-    type: "text" | "number" | "email" | "password";
+    type: HTMLInputTypeAttribute;
     label?: string;
     value: string | number;
     placeholder?: string;
@@ -12,10 +12,8 @@ export type Tprops = {
     disabled?: boolean;
     preIcon?: React.ReactNode;
     postIcon?: React.ReactNode;
-    onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
-    size: "sm" | "md" | "lg";
-    supportingText: string;
-    status?: "disabled" | "error" | "Success";
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    supportingText?: string;
 };
 function Input({
     id,
@@ -23,7 +21,6 @@ function Input({
     onChange,
     preIcon,
     postIcon,
-    size,
     type,
     label,
     value,
@@ -39,18 +36,8 @@ function Input({
             email: " block text-sm font-medium mb-1",
             password: "  block text-sm font-medium mb-1",
         },
-        status: {
-            disabled:
-                "form-input w-full disabled:border-slate-200 disabled:bg-slate-100 disabled:text-slate-400 disabled:cursor-not-allowed",
-            error: "form-input w-full border-rose-300",
-            success: "form-input w-full border-emerald-300",
-        },
     };
-    const sizes = {
-        sm: "w-full max-w-[288px]",
-        md: "w-full max-w-[288px]",
-        lg: "w-full max-w-[288px]",
-    };
+
     return (
         <>
             <div className="pt-4">
@@ -64,9 +51,8 @@ function Input({
                     value={value}
                     onChange={onChange}
                     disabled={disabled}
-                    className={`form-input ${
-                        sizes[size]
-                    }  px-3 py-2 placeholder 
+                    className={`form-input 
+                      px-3 py-2 placeholder 
                         ${preIcon ? "ps-10" : postIcon ? "pe-10" : ""}
                         `}
                 />
