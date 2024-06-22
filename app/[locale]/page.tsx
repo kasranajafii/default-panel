85c319eb2171dcdfbdba1ba06020b4c21bc374a0
kasranajafii <kasranajafi40@gmail.com>
2024-06-22 17:32:39 +0330

    fix/cruip-switchbutton

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,11 +1,5 @@
 "use client";
 
-import SwitchButton from "@/components/cuipe/SwitchButton";
-
 export default function Home() {
-    return (
-        <main className="p-5">
-            <SwitchButton label="parhma" />
-        </main>
-    );
+    return <main className="p-5"></main>;
 }
